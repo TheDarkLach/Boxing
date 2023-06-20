d08bb73c3348b5afd77c933cc45848894496747d
Faizan Karim <50496663+TheDarkLach@users.noreply.github.com>
2023-06-20 13:50:15 -0700

    test

--- src/main/java/lach/plugin/listeners/arenaListen.java
+++ src/main/java/lach/plugin/listeners/arenaListen.java
@@ -20,18 +20,9 @@ public class arenaListen extends JavaPlugin implements Listener
     private RegionSelector regionSelector;
     public void onEnable()
     {
-        WorldEdit worldEdit = WorldEdit.getInstance();
-        this.regionSelector = worldEdit.getSessionManager().get(BukkitAdapter.adapt(player)).getRegionSelector(BukkitAdapter.adapt(player.getWorld()));
         getServer().getPluginManager().registerEvents(this, this);
     }
     public void onPlayerMove(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
-        Location location = player.getLocation();
-        BlockVector3 blockVector3 = BlockVector3.at(location.getX(), location.getY(), location.getZ());
-        if (regionSelector.contains(blockVector3)) {
-            // The player has entered the region
-            // Do something here, e.g. send a message to the player
-            player.sendMessage("You have entered the region!");
-        }
+        // here we will check if they are on a certain block
     }
 }
